95b6d8818b6a36a5b1bfa7da4d627a908db816a6
SWARNA PRABHU <swarna.prabhu@intel.com>
2022-07-08 13:59:06 -0700

    latest4 commit

--- pre-check.sh
+++ pre-check.sh
@@ -4,7 +4,7 @@
 
 ## a simple script to enable the right toplogy path for test cases before they are executed.
 
-## disabled the sof logger dependency 
+## disabled the sof logger dependency as it is not relevant to audio test
 
 
  export TPLG="/lib/firmware/intel/sof-tplg/redrix/sof-adl-max98390-rt5682.tplg"
